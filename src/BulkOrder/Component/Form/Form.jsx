2209c9dea64f5b3c9607c3057c707a28d18659ee
QuarterColon <ankitchakraborty1972@gmail.com>
2024-02-20 05:13:53 +0530

    tablet response

--- src/BulkOrder/Component/Form/Form.jsx
+++ src/BulkOrder/Component/Form/Form.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 import "./Form.css";
 import {
   setOrders,
@@ -10,7 +9,7 @@ import {
 import PopUp from "../../../customs/popup";
 import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../../../LandingPage/Components/Navbar/navbar";
-import  {OrderFiller}  from "../../../../src/assets/index.js";
+import { OrderFiller } from "../../../../src/assets/index.js";
 const Form = () => {
   const [org, setOrg] = useState();
   const [contact, setContact] = useState();
@@ -24,7 +23,7 @@ const Form = () => {
   const [heading, setHeading] = useState("");
   const [modalContent, setModalContent] = useState();
   const navigate = useNavigate();
-  const {category} = useParams();
+  const { category } = useParams();
 
   const submitHandler = async () => {
     if (
@@ -61,97 +60,97 @@ const Form = () => {
 
   return (
     <>
-    <Navbar/>
-    <form>
-      <PopUp onClose={closeHandler} show={visibility} title={heading}>
-        {modalContent}
-      </PopUp>
+      <Navbar />
+      <form>
+        <PopUp onClose={closeHandler} show={visibility} title={heading}>
+          {modalContent}
+        </PopUp>
 
-      <col-left>
-        <h1>Order Details</h1>
-        <order-details>
-          <input-field>
-            <label htmlFor="name">Club/Project Name:</label>
-
-            <input
-              type="text"
-              id="name"
-              onChange={(e) => setOrg(e.target.value)}
-            />
-          </input-field>
-
-          <input-field>
-            <label htmlFor="contact">Contact Number:</label>
-            <input
-              type="text"
-              id="contact"
-              onChange={(e) => setContact(e.target.value)}
-            ></input>
-          </input-field>
-
-          <input-field>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-          </input-field>
-        </order-details>
-        <shipping-details>
-          <h1>Shipping Details</h1>
-          <input-field>
-            <label htmlFor="address">Shipping Address:</label>
-            <input
-              type="text"
-              id="address"
-              onChange={(e) => setAddress(e.target.value)}
-            ></input>
-          </input-field>
-        </shipping-details>
-      </col-left>
-
-      <border-line></border-line>
-
-      <col-right>
-        <filler-image>
-          <img src={OrderFiller} alt="" />
-        </filler-image>
-        <design-details>
-          <h1>Design Details</h1>
-          <input-field>
-            <label htmlFor="order quantity">Order quantity:</label>
-            <input
-              type="number"
-              id="quantity"
-              onChange={(e) => setQuantity(e.target.value)}
-            ></input>
-          </input-field>
-
-          <input-field>
-            <label htmlFor="instructions">Specific Instructions:</label>
-            <input
-              type="text"
-              id="instructions"
-              onChange={(e) => setInstructions(e.target.value)}
-            ></input>
-          </input-field>
-          <upload-files>
+        <col-left>
+          <order-details>
+            <h1>Order Details</h1>
             <input-field>
-              <label htmlFor="details">Tshirt details (.xls):</label>
-              <upload-input>
-                <file-name>{details?.name}</file-name>
-                <label className="upload-file" htmlFor="formId1">
-                  Upload
-                  <input
-                    name=""
-                    type="file"
-                    id="formId1"
-                    hidden
-                    onChange={(e) => setDetails(e.target.files[0])}
-                  />
-                </label>
-                {/* <input
+              <label htmlFor="name">Club/Project Name:</label>
+
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setOrg(e.target.value)}
+              />
+            </input-field>
+
+            <input-field>
+              <label htmlFor="contact">Contact Number:</label>
+              <input
+                type="text"
+                id="contact"
+                onChange={(e) => setContact(e.target.value)}
+              ></input>
+            </input-field>
+
+            <input-field>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </input-field>
+          </order-details>
+          <shipping-details>
+            <h1>Shipping Details</h1>
+            <input-field>
+              <label htmlFor="address">Shipping Address:</label>
+              <input
+                type="text"
+                id="address"
+                onChange={(e) => setAddress(e.target.value)}
+              ></input>
+            </input-field>
+          </shipping-details>
+        </col-left>
+
+        <border-line></border-line>
+
+        <col-right>
+          <filler-image>
+            <img src={OrderFiller} alt="" />
+          </filler-image>
+          <design-details>
+            <h1>Design Details</h1>
+            <input-field>
+              <label htmlFor="order quantity">Order quantity:</label>
+              <input
+                type="number"
+                id="quantity"
+                onChange={(e) => setQuantity(e.target.value)}
+              ></input>
+            </input-field>
+
+            <input-field>
+              <label htmlFor="instructions">Specific Instructions:</label>
+              <input
+                type="text"
+                id="instructions"
+                onChange={(e) => setInstructions(e.target.value)}
+              ></input>
+            </input-field>
+            <upload-files>
+              <input-field>
+                <label htmlFor="details">Tshirt details (.xls):</label>
+                <upload-input>
+                  <file-name>{details?.name}</file-name>
+                  <label className="upload-file" htmlFor="formId1">
+                    Upload
+                    <input
+                      name=""
+                      type="file"
+                      id="formId1"
+                      hidden
+                      onChange={(e) => setDetails(e.target.files[0])}
+                    />
+                  </label>
+                  {/* <input
                   onChange={handleChangeDetails}
                   ref={hiddenFileInput1}
                   type="file"
@@ -164,46 +163,46 @@ const Form = () => {
                     type="file"
                   />
                 </button> */}
-              </upload-input>
-            </input-field>
-          </upload-files>
+                </upload-input>
+              </input-field>
+            </upload-files>
 
-          <upload-files>
-            <input-field>
-              <label htmlFor="design">Design (.jpg):</label>
-              <upload-input>
-                <file-name>{design?.name}</file-name>
-                <label className="upload-file" htmlFor="formId2">
-                  Upload
-                  <input
-                    name=""
-                    type="file"
-                    id="formId2"
-                    hidden
-                    onChange={(e) => setDesign(e.target.files[0])}
-                  />
-                </label>
+            <upload-files>
+              <input-field>
+                <label htmlFor="design">Design (.jpg):</label>
+                <upload-input>
+                  <file-name>{design?.name}</file-name>
+                  <label className="upload-file" htmlFor="formId2">
+                    Upload
+                    <input
+                      name=""
+                      type="file"
+                      id="formId2"
+                      hidden
+                      onChange={(e) => setDesign(e.target.files[0])}
+                    />
+                  </label>
 
-                {/* <input
+                  {/* <input
                   type="file"
                   ref={hiddenFileInput2}
                   onChange={handleChangeDesign}
                   
                 />
                 <button onClick={handleClick2}>Upload</button> */}
-              </upload-input>
-            </input-field>
-          </upload-files>
-          <submit-button>
-            <button onClick={submitHandler} type="button" formTarget="_top">
-              Submit
-            </button>
-          </submit-button>
-        </design-details>
-      </col-right>
+                </upload-input>
+              </input-field>
+            </upload-files>
+            <submit-button>
+              <button onClick={submitHandler} type="button" formTarget="_top">
+                Submit
+              </button>
+            </submit-button>
+          </design-details>
+        </col-right>
 
-      <input-fields></input-fields>
-    </form>
+        <input-fields></input-fields>
+      </form>
     </>
   );
 };
