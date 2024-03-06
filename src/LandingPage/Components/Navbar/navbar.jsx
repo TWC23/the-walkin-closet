9c2037780689da55c22f604610f89cfd1cbd3602
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-03-07 01:05:53 +0530

    Add login dropdown menu to navbar

--- src/LandingPage/Components/Navbar/navbar.jsx
+++ src/LandingPage/Components/Navbar/navbar.jsx
@@ -1,76 +1,111 @@
-import React from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom'
-import {bag, Profile, favorite, search, logo, walkincloset, UpdatedLogo} from '../../../assets/index'
-import './navbar.css'
+// react imports
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+// toast imports
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// asset imports
+import { bag, Profile, favorite, search, logo, walkincloset, UpdatedLogo } from '../../../assets/index';
+
+// component imports
+import DropdownMenu from "../DropdownMenu/dropdown";
+
+// css imports
+import "./navbar.css";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const notify = () => toast.info("Coming soon!");
+  const navigate = useNavigate();
+  const notify = () => toast.info("Coming soon!");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    return (
-        <nav>
-            <a href="/" onClick={() => navigate("/")}>
-                <img src={UpdatedLogo} alt="TWC logo" className='logo-img' />
-            </a>
-            <ul>
-                <li>
-                    <a href="#" onClick={notify}>Men</a>
-                </li>
-                <li>
-                    <a href="#" onClick={notify}>Women</a>
-                </li>
-                <li>
-                    <a href="#" onClick={notify}>Blog</a>
-                </li>
-                <li>
-                    <a href="#" onClick={notify}>Originals</a>
-                </li>
-                <li>
-                    <a href="#" onClick={notify}>Boutique Boulevard</a>
-                </li>
-                <li>
-                    <a href="#" onClick={notify}>Rentals</a>
-                </li>
-                <li>
-                    <a href="/bulk-order" onClick={() => navigate("/bulk-order")}>
-                        College Merch
-                    </a>
-                </li>
-            </ul>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition:Bounce
-            />
+  return (
+    <nav>
+      <a href="/" onClick={() => navigate("/")}>
+        <img src={UpdatedLogo} alt="TWC logo" className="logo-img" />
+      </a>
+      <ul>
+        <li>
+          <a href="#" onClick={notify}>
+            Men
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={notify}>
+            Women
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={notify}>
+            Blog
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={notify}>
+            Originals
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={notify}>
+            Boutique Boulevard
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={notify}>
+            Rentals
+          </a>
+        </li>
+        <li>
+          <a href="/bulk-order" onClick={() => navigate("/bulk-order")}>
+            College Merch
+          </a>
+        </li>
+        <li>
+          <div
+            className="menu"
+            onMouseEnter={toggleDropdown}
+            onMouseLeave={toggleDropdown}
+          >
+            <img src={Profile} alt="" />
+            {/* <DropdownMenu /> */}
+            {isDropdownOpen && <DropdownMenu />}
+          </div>
+        </li>
+      </ul>
 
-            {/* <ul>
-                <li>
-                    <img src={search} alt="" />
-                </li>
-                
-                <li>
-                    <img src={favorite} alt="" />
-                </li>
-                <li>
-                    <img src={bag} alt="" />
-                </li>
-                <li>
-                    <img src={Profile} alt="" />
-                </li>
-            </ul> */}
-        </nav>
-    )
-}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition:Bounce
+      />
 
-export default Navbar
+      {/* <ul>
+        <li>
+          <img src={search} alt="" />
+        </li>
+
+        <li>
+          <img src={favorite} alt="" />
+        </li>
+        <li>
+          <img src={bag} alt="" />
+        </li>
+      </ul> */}
+    </nav>
+  );
+};
+
+export default Navbar;
