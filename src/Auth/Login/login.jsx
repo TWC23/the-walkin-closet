2b2939830590958b2217f2f971522bf0c3333664
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-03-07 15:29:19 +0530

    fix redirect post login

--- src/Auth/Login/login.jsx
+++ src/Auth/Login/login.jsx
@@ -5,88 +5,102 @@ import { auth } from "../../firebase-config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Navbar from "../../LandingPage/Components/Navbar/navbar";
 import Footer from "../../LandingPage/Components/Footer/footer";
-import './login.css';
+import "./login.css";
 
 const Login = () => {
-    const [loginEmail, setLoginEmail] = useState();
-    const [loginPassword, setLoginPassword] = useState();
+  const [loginEmail, setLoginEmail] = useState();
+  const [loginPassword, setLoginPassword] = useState();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const login = async () => {
-        try {
-            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-            navigate("/comingsoon");
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-    
-    const googleLogin = () => {
-        signInWithGoogle()
-        .then((result) => {
-                navigate("/comingsoon");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    return (
-        <>
-            <div className="navbar">
-                <Navbar />
-            </div>
+  const googleLogin = () => {
+    signInWithGoogle()
+      .then((result) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-            <div className="loginBase">
-                <div className="loginContainer">
-                    <h1>Login</h1>
-                    
-                    <div className="input-container">
-                        <label>Username</label>
-                        <input
-                            type="text"
-                            placeholder="Enter your username"
-                            onChange={(event) => {
-                                setLoginEmail(event.target.value);
-                            }}
-                            required />
-                    </div>
-                    <div className="input-container">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter your password"
-                            onChange={(event) => {
-                                setLoginPassword(event.target.value);
-                            }}
-                            required />
-                    </div>
+  return (
+    <>
+      <div className="navbar">
+        <Navbar />
+      </div>
 
-                    <a href="#" className="forgot-password">Forgot password</a>
+      <div className="loginBase">
+        <div className="loginContainer">
+          <h1>Login</h1>
 
-                    <button className="login-button" onClick={ login }><p>Login</p></button>
-                    <button className="google-login" onClick = { googleLogin }>
-                        <img
-                        src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
-                        alt="Trees"
-                        height="30"
-                        />
-                        <p>Sign in with Google</p>
-                    </button>
+          <div className="input-container">
+            <label>Username</label>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              onChange={(event) => {
+                setLoginEmail(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              onChange={(event) => {
+                setLoginPassword(event.target.value);
+              }}
+              required
+            />
+          </div>
 
-                    <div className="link-to-signup">
-                        <p>Don&apos;t have an account? <a href="/signup" onClick={() => navigate("/signup")}>Sign up</a></p>
-                    </div>
-                    
-                </div>
-            </div>
+          <a href="#" className="forgot-password">
+            Forgot password
+          </a>
 
-            <div className="footer">
-                <Footer />
-            </div>
-        </>
-    )
-}
+          <button className="login-button" onClick={login}>
+            <p>Login</p>
+          </button>
+          <button className="google-login" onClick={googleLogin}>
+            <img
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+              alt="Trees"
+              height="30"
+            />
+            <p>Sign in with Google</p>
+          </button>
+
+          <div className="link-to-signup">
+            <p>
+              Don&apos;t have an account?{" "}
+              <a href="/signup" onClick={() => navigate("/signup")}>
+                Sign up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default Login;
--- src/Auth/Signup/signup.jsx
+++ src/Auth/Signup/signup.jsx
@@ -5,99 +5,105 @@ import { useNavigate } from "react-router-dom";
 import { signInWithGoogle } from "../../FirebaseFunctions/firebase";
 import Navbar from "../../LandingPage/Components/Navbar/navbar";
 import Footer from "../../LandingPage/Components/Footer/footer";
-import './signup.css';
+import "./signup.css";
 
 const SignUp = () => {
-    const [signupEmail, setSignupEmail] = useState();
-    const [signupPassword, setSignupPassword] = useState();
-    const navigate = useNavigate();
+  const [signupEmail, setSignupEmail] = useState();
+  const [signupPassword, setSignupPassword] = useState();
+  const navigate = useNavigate();
 
-    const googleLogin = () => {
-        signInWithGoogle()
-            .then((result) => {
-                navigate("/comingsoon");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const googleLogin = () => {
+    signInWithGoogle()
+      .then((result) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const signup = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        signupEmail,
+        signupPassword
+      );
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    const signup = async () => {
-        try {
-            const user = await createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
-            navigate("/comingsoon");
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-    
-    return (
-        <>
-            <div className="navbar">
-                <Navbar />
-            </div>
+  return (
+    <>
+      <div className="navbar">
+        <Navbar />
+      </div>
 
-            <div className="signupBase">
-                <div className="signupContainer">
-                    <h1>Create Account</h1>
-                    
-                    <div className="input-container">
-                        <label>Name</label>
-                        <input
-                            type="text"
-                            placeholder="John Doe"
-                            required />
-                    </div>
-                    <div className="input-container">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            placeholder="example@gmail.com"
-                            onChange={(event) => {
-                                setSignupEmail(event.target.value);
-                            }}
-                            required />
-                    </div>
-                    <div className="input-container">
-                        <label>Phone Number</label>
-                        <input
-                            type="text"
-                            placeholder="XXXXXXXXXX"
-                            required />
-                    </div>
-                    <div className="input-container">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            placeholder="Create password"
-                            onChange={(event) => {
-                                setSignupPassword(event.target.value);
-                            }}
-                            required />
-                    </div>
+      <div className="signupBase">
+        <div className="signupContainer">
+          <h1>Create Account</h1>
 
-                    <button className="signup-button" onClick={signup}><p>Create Account</p></button>
-                    <button className="google-login" onClick={ googleLogin }>
-                        <img
-                        src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
-                        alt="Trees"
-                        height="30"
-                        />
-                        <p>Sign in with Google</p>
-                    </button>
+          <div className="input-container">
+            <label>Name</label>
+            <input type="text" placeholder="John Doe" required />
+          </div>
+          <div className="input-container">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              onChange={(event) => {
+                setSignupEmail(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Phone Number</label>
+            <input type="text" placeholder="XXXXXXXXXX" required />
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Create password"
+              onChange={(event) => {
+                setSignupPassword(event.target.value);
+              }}
+              required
+            />
+          </div>
 
-                    <div className="link-to-login">
-                        <p>Already have an account? <a href="/login" onClick={() => navigate("/login")}>Login</a></p>
-                    </div>
-                    
-                </div>
-            </div>
+          <button className="signup-button" onClick={signup}>
+            <p>Create Account</p>
+          </button>
+          <button className="google-login" onClick={googleLogin}>
+            <img
+              src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+              alt="Trees"
+              height="30"
+            />
+            <p>Sign in with Google</p>
+          </button>
 
-            <div className="footer">
-                <Footer />
-            </div>
-        </>
-    )
-}
+          <div className="link-to-login">
+            <p>
+              Already have an account?{" "}
+              <a href="/login" onClick={() => navigate("/login")}>
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
